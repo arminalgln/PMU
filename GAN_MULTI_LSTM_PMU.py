b4bf4ed008822e283455a3d4f8e8e40cbe5a71c4
armin <armin.aligholian@gmail.com>
2019-07-11 20:02:04 -0700

    add multi LSTM with 2 additional dense layer, I should implement PCA, test for whole day

---
+++ GAN_MULTI_LSTM_PMU.py
@@ -0,0 +1,303 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+import keras
+from keras.layers import Dense, Dropout, Input, Embedding, LSTM, Reshape
+from keras.models import Model,Sequential
+from keras.datasets import mnist
+from tqdm import tqdm
+from keras.layers.advanced_activations import LeakyReLU
+from keras.activations import relu
+from keras.optimizers import adam
+import numpy as np
+import tensorflow as tf
+import os
+import pickle
+import operator
+import math
+from sklearn import preprocessing
+from keras.models import load_model
+
+#%% 
+def load_data(start,SampleNum,N):
+         #read a pickle file
+    pkl_file = open('CompleteOneDay.pkl', 'rb')
+    selected_data = pickle.load(pkl_file)
+    pkl_file.close()
+    for pmu in ['1224']:
+        selected_data[pmu]=pd.DataFrame.from_dict(selected_data[pmu])
+    features=['L2MAG', 'L1MAG', 'C1MAG', 'L3MAG',
+       'C2MAG', 'C3MAG', 'PA', 'PB', 'PC', 'QA', 'QB', 'QC',]
+    
+    select=[]
+    for f in features:
+        select.append(selected_data['1224'][f].iloc[0:int(N*SampleNum/2)+20].values)
+    
+    select=np.array(select)
+    
+    select=preprocessing.scale(select,axis=1)
+    
+    selected_data=0
+    end=start+SampleNum
+    pmu='1224'
+    shift=int(SampleNum/2)
+    
+    train_data=np.zeros((N,12,SampleNum))
+#    reduced_mean=np.zeros((12,20))
+    for i in range(N):
+        if i% 1000==0:
+            print('iter num: %i', i)
+        temp=select[:,start+i*shift:end+i*shift] 
+        temp=(temp-temp.mean(axis=1).reshape(-1,1)) ## reduced mean
+#        temp = preprocessing.scale(temp,axis=1)  ## standardized
+#        reduced_mean=np.concatenate((reduced_mean,temp[:,0:20]),axis=1)
+        train_data[i,:]=temp
+    
+    
+    # convert shape of x_train from (60000, 28, 28) to (60000, 784) 
+    # 784 columns per row
+    
+    return train_data,select#,select_proc,reduced_mean
+#X_train=load_data()
+#print(X_train.shape)
+#%%
+def adam_optimizer():
+    return adam(lr=0.0002, beta_1=0.5)
+#%%
+def create_generator():
+    generator=Sequential()
+    generator.add(LSTM(units=256,input_shape=(100,1)))
+    generator.add(LeakyReLU(0.2))
+    
+    generator.add(Dense(units=512))
+    generator.add(LeakyReLU(0.2))
+#    
+#    generator.add(LSTM(units=1024))
+#    generator.add(LeakyReLU(0.2))
+    
+    generator.add(Dense(units=12*40))
+    
+    generator.compile(loss='binary_crossentropy', optimizer=adam_optimizer())
+    return generator
+g=create_generator()
+g.summary()
+
+#%%
+def create_discriminator():
+    discriminator=Sequential()
+    discriminator.add(LSTM(units=256,input_shape=(40,12)))
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(Dropout(0.3))
+       
+#    
+    discriminator.add(Dense(units=512))
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(Dropout(0.3))
+#       
+#    discriminator.add(LSTM(units=256))
+#    discriminator.add(LeakyReLU(0.2))
+    
+    discriminator.add(Dense(units=1, activation='sigmoid'))
+    
+    discriminator.compile(loss='binary_crossentropy', optimizer=adam_optimizer())
+    return discriminator
+d =create_discriminator()
+d.summary()
+#%%
+def create_gan(discriminator, generator):
+    discriminator.trainable=False
+    gan_input = Input(shape=(100,1))
+    x = generator(gan_input)
+    x = Reshape((40,12), input_shape=(12*40,1))(x)
+    gan_output= discriminator(x)
+    gan= Model(inputs=gan_input, outputs=gan_output)
+    gan.compile(loss='binary_crossentropy', optimizer='adam')
+    return gan
+gan = create_gan(d,g)
+gan.summary()
+
+#%%
+def plot_generated_images(epoch, generator, examples=100, dim=(10,10), figsize=(10,10)):
+    scale=1
+    noise= scale*np.random.normal(loc=0, scale=1, size=[examples, 100])
+    generated_images = generator.predict(noise)
+    generated_images = generated_images.reshape(100,40,1)
+    plt.figure(figsize=figsize)
+    for i in range(generated_images.shape[0]):
+        plt.subplot(dim[0], dim[1], i+1)
+        plt.plot(generated_images[i])
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('gan_generated_image %d.png' %epoch)
+    return generated_images
+    
+#%%
+batch_size=10
+epochnum=100
+#%%
+start,SampleNum,N=(0,40,1000)
+X_train, selected = load_data(start,SampleNum,N)
+batch_count = X_train.shape[0] / batch_size
+#%%
+X_train=X_train.reshape(N,12*SampleNum)
+X_train=X_train.reshape(N,SampleNum,12)
+#%%
+generator= create_generator()
+discriminator= create_discriminator()
+gan = create_gan(discriminator, generator)
+#%%
+def training(generator,discriminator,gan,epochs, batch_size):
+    scale=1
+    for e in range(1,epochs+1 ):
+        print("Epoch %d" %e)
+        for _ in tqdm(range(batch_size)):
+        #generate  random noise as an input  to  initialize the  generator
+            noise= scale*np.random.normal(0,1, [batch_size, 100])
+            noise=noise.reshape(batch_size,100,1)
+            # Generate fake MNIST images from noised input
+            generated_images = generator.predict(noise)
+            generated_images = generated_images.reshape(batch_size,SampleNum,12)
+#            print(generated_images.shape)
+            # Get a random set of  real images
+            image_batch =X_train[np.random.randint(low=0,high=X_train.shape[0],size=batch_size)]
+#            print(image_batch.shape)
+            #Construct different batches of  real and fake data 
+            X= np.concatenate([image_batch, generated_images])
+            
+            # Labels for generated and real data
+            y_dis=np.zeros(2*batch_size)
+            y_dis[:batch_size]=0.9
+            
+            #Pre train discriminator on  fake and real data  before starting the gan. 
+            discriminator.trainable=True
+            discriminator.train_on_batch(X, y_dis)
+            
+            #Tricking the noised input of the Generator as real data
+            noise= scale*np.random.normal(0,1, [batch_size, 100])
+            noise=noise.reshape(batch_size,100,1)
+            y_gen = np.ones(batch_size)
+            
+            # During the training of gan, 
+            # the weights of discriminator should be fixed. 
+            #We can enforce that by setting the trainable flag
+            discriminator.trainable=False
+            
+            #training  the GAN by alternating the training of the Discriminator 
+            #and training the chained GAN model with Discriminator’s weights freezed.
+            gan.train_on_batch(noise, y_gen)
+            
+#        if e == 1 or e % 5 == 0:
+#           
+            
+#            plot_generated_images(e, generator)
+#batch_size=0
+
+training(generator,discriminator,gan,epochnum,batch_size)
+
+
+
+#%%
+
+gan.save('gan_mul_LSTM_N1000_e100_b10.h5')
+generator.save('generator_mul_LSTM_N1000_e100_b10.h5')
+discriminator.save('discriminator_mul_LSTM_N1000_e100_b10.h5')
+#%%
+
+start,SampleNum,N=(0,40,500000)
+X_train, selected = load_data(start,SampleNum,N)
+batch_count = X_train.shape[0] / batch_size
+
+#%%
+X_train=X_train.reshape(N,12*SampleNum)
+X_train=X_train.reshape(N,SampleNum,12)
+#%%
+a=discriminator.predict_on_batch(X_train)
+
+#%%
+fig_size = plt.rcParams["figure.figsize"]
+ 
+ 
+# Set figure width to 12 and height to 9
+fig_size[0] = 8
+fig_size[1] = 6
+plt.plot(a.ravel())
+plt.show()
+
+
+#%%
+high=.94
+low=0.26
+
+fig_size = plt.rcParams["figure.figsize"]
+ 
+ 
+# Set figure width to 12 and height to 9
+fig_size[0] = 8
+fig_size[1] = 6
+anoms=np.union1d(np.where(a>=high)[0], np.where(a<=low)[0])
+print(np.union1d(np.where(a>=high)[0], np.where(a<=low)[0]).shape)
+tt=X_train.reshape(N,12*SampleNum)
+tt=X_train.reshape(N,12,SampleNum)
+#%%
+
+normal=np.arange(100,110)
+for i in anoms :
+    for j in range(12):
+        plt.plot(tt[i][j])
+    plt.legend(('vol', 'curr', 'p','q'),shadow=True, loc=(0.01, 0.48), handlelength=1.5, fontsize=16)
+    plt.show()
+
+#%%
+selected=pd.DataFrame(selected)
+selected=selected.T
+
+#%%
+fig_size = plt.rcParams["figure.figsize"]
+ 
+ 
+# Set figure width to 12 and height to 9
+fig_size[0] = 10
+fig_size[1] = 8
+plt.rcParams["figure.figsize"] = fig_size
+start=0
+dur=N*20
+end=start+dur
+selected['color']='b'
+for i in anoms:
+#    print(i)
+    selected['color'].iloc[i*int(SampleNum/2):((i+1)*int(SampleNum/2)+40)]='r'
+
+markers_on=np.where(selected['color'].iloc[start:end]=='r')
+#plt.plot(selected[0].iloc[start:end], markevery=list(markers_on),marker='X',mec='r',mew=np.log(np.log(dur))
+#    ,ms=2*np.log(np.log(dur)),mfcalt='r')
+#for i in range(5):
+#    plt.plot(selected[i].iloc[start:end])
+#    plt.show()
+for j in [1,2,6,9]:
+    plt.plot(selected[j].iloc[start:end])
+#    plt.xlabel('timeslots',fontsize=28)
+#    plt.ylabel('phase 1 current magnitude pmu="1024"',fontsize=28)
+    for i in anoms:
+        if (i*int(SampleNum/2)+1) in list(np.arange(start,end)):
+            plt.axvspan(i*int(SampleNum/2), ((i+1)*int(SampleNum/2)+40), color='red', alpha=0.5)
+    plt.show()
+#plt.savefig('long.pdf', format='pdf', dpi=1200)
+#plt.savefig('long %d.png' %dur)
+#%%
+dur_anoms=[]
+for i in anoms:
+    if (i*int(SampleNum/2)+1) in list(np.arange(start,end)):
+        dur_anoms.append([i*int(SampleNum/2),((i+1)*int(SampleNum/2)+20)])
+        plt.plot(selected[2].iloc[i*int(SampleNum/2)-20:((i+1)*int(SampleNum/2)+40)].values)
+        plt.xlabel('timeslots',fontsize=28)
+        plt.ylabel('phase 1 current magnitude pmu="1024"',fontsize=28)
+#        plt.savefig('figures/event %d.png' %i)
+#        plt.savefig('figures/event %d.pdf' %i, format='pdf', dpi=1200)
+        plt.show()
+
+print(dur_anoms)
+print(len(dur_anoms))
+
---
+++ q
@@ -0,0 +1,77 @@
+[33mcommit df9dfc8a538b5d5d29d1a56061c56d8dbab7bea2[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Wed Jul 10 21:59:41 2019 -0700
+
+    best result so far simple GAN N=10000,batch=100, epoch=200
+
+[33mcommit 1e2acc35f7ab37b5e5fc45d48e29e30419011e1d[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Wed Jul 10 15:43:50 2019 -0700
+
+    multivariate get fixed and plots for anomalies have been showns, Q and I seems to be prominant
+
+[33mcommit fffa49bd9307405c89f4e28be21dc24c93f57e94[m[33m ([m[1;32mold_state[m[33m)[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Mon Jul 8 23:42:55 2019 -0700
+
+    change data to multivariate and first test with multi, has some problem in ploting, normalized data have been used instead of reduced mean
+
+[33mcommit 5a98da914b27bbb2e49b0cefd0616dd4a7102f5d[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Wed Jul 3 21:02:14 2019 -0700
+
+     Run then model for 2 h 20 m and obtain 29 events and highlight the events inside of each time preiod automaticaly
+
+[33mcommit ef5b6c8c68d30c9f075736ccb99c24967ee6c9b3[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Tue Jul 2 19:29:20 2019 -0700
+
+    GAN model learning added
+
+[33mcommit 7baf6375b3b3cea601e73042e2835aceaf6371a7[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Sun Jun 30 21:27:17 2019 -0700
+
+    first simple knn clustering
+
+[33mcommit 2004e826ccf04bedac597a228071c959037e47e2[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Sat Jun 29 12:29:10 2019 -0700
+
+    reduce the mean of each subsequence of data series, to see the events easily. mean should be readuced for each subsequence not for whole data
+
+[33mcommit bd5b7a231ba32364eb2d749e80d1ae13e6715f88[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Wed Jun 26 19:54:58 2019 -0700
+
+    just ploting some figures to see the events and apply matrix profile in matlab
+
+[33mcommit 138909642e37755622bef54b85d4abc994351319[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Tue Jun 25 19:44:51 2019 -0700
+
+    apply IF to the one PMU data
+
+[33mcommit d97eef81f79a39b7e9f9034d0ca2a634cd951588[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Tue Jun 25 18:37:57 2019 -0700
+
+    calculating active and reactive power and PF, make file to anayze the sorted data
+
+[33mcommit 733cd8349f23449cea898bdc1aab46f85a09ce77[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Tue Jun 25 14:48:24 2019 -0700
+
+    seperating the data for one day wrt PMU location and append them for all day for each 10 minutes and save them in a pkl file
+
+[33mcommit 83d3f91985b6dda910b21a424c471f618a551f68[m
+Author: armin <armin.aligholian@gmail.com>
+Date:   Tue Jun 25 12:23:42 2019 -0700
+
+    make a spyder project from scratch
+
+[33mcommit 8de0423f129f0e7392b0fec7ee40ec638ca3a284[m
+Author: arminalgln <armin.aligholian@gmail.com>
+Date:   Tue Jun 25 12:08:22 2019 -0700
+
+    Initial commit
